401c11810f211c6b24bea208aa138f0c59ccd753
Adrian Anton <adrtler@gmail.com>
2020-03-16 17:37:34 +0100

    Sección 12 terminada backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = '@este-es@_un-seed-dificil';
+
+// Google
+
+module.exports.CLIENT_ID = '99449547660-t5kfi7legjtdeeiabbcc50f3cgu02pdq.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -3,10 +3,128 @@ var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 var SEED = require('../config/config').SEED;
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
 
 var app = express();
 var Usuario = require('../models/usuario');
 
+/**
+ * Login google
+ */
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    // Aquí está toda la información del usuario
+    const payload = ticket.getPayload();
+    // const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true,
+        payload
+    };
+}
+
+app.post('/google', async(req, res) => {
+    var token = req.body.token;
+    var googleUser = await verify(token).catch((e) => {
+        if (e) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Token no válido'
+            });
+        }
+    });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuarios login google',
+                errors: err
+            });
+        }
+        // Entonces ya existe
+        if (usuarioDB) {
+            /**
+             * Si el correo ya estaba registrado y no fue por la autenticación por google
+             * hay que sacarlo
+             */
+            if (usuarioDB.google === false) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Debe usar su autenticación normal'
+                });
+            } else {
+                /**
+                 * Si el usuario existe hacemos login
+                 */
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                /**
+                 * Devolvemos el token que luego el usuario nos tiene que mandar para poder hacer las operaciones
+                 */
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            }
+        } else {
+            /**
+             * El usuario no existe, hay que crearlo
+             */
+            var usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':P';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al guardar usuario base de datos login google',
+                        errors: err
+                    });
+                }
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                /**
+                 * Devolvemos el token que luego el usuario nos tiene que mandar para poder hacer las operaciones
+                 */
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            });
+        }
+    });
+});
+
+
+/**
+ * Login normal
+ */
 app.post('/', (req, res) => {
     var body = req.body;
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
